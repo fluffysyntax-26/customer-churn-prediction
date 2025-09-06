ac002812fb4e0e8d2832137cf77918185a3a7afb
Deepak <www.deepakkrishna9845@gmail.com>
2025-09-06 18:11:19 +0530

    re-ran the project to update pickle files and the keras model, added requirement.txt

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = load_model('/home/dpxk/Desktop/customer-churn-prediction/model.keras')
 
 # Load the encoders and scaler
 with open('gender_le.pkl', 'rb') as file: 
@@ -36,15 +37,16 @@ input_data = pd.DataFrame({
     'CreditScore': [credit_score],
     'Gender': [gender_le.transform([gender])[0]],
     'Age': [age],
-    'Balance': [balance],
     'Tenure': [tenure],
+    'Balance': [balance],
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
-    'IsActiveMember': [is_active_member]
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
 })
 
 # One-hot encode 'Geography'
-geo_encoded = geography_ohe.transform(pd.DataFrame({'Geography': [geography]})).toarray()
+geo_encoded = geography_ohe.transform(pd.DataFrame({'Geography': [geography]}))
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=geography_ohe.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
